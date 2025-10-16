d3cf031628e4d1051c2f8b8e814190cd5abb1198
vdutts <github.relock416@passmail.net>
2025-10-16 01:19:33 -0700

    better ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import './app.css'
 
@@ -21,7 +20,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans antialiased`}>
         {children}
-        <Analytics />
       </body>
     </html>
   )
